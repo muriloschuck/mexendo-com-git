907bcba0801d51da8ab7d7fd6ada48d73585d312
murilo schuck <murilo.schuck2@gmail.com>
2021-08-25 21:07:06 -0300

    testing a new branch

--- hi.py
+++ hi.py
@@ -6,8 +6,13 @@ x = int(input())
 print(dummyfunction(x))
 
 def idkPythonAnymore(n, m):
-    return n*m
+    return n+m
 
 y = int(input())
 
 print(idkPythonAnymore(x,y))
+
+if y > 2:
+    print("PlodrinyZ")
+else: 
+    print("Pedrokinha14")
